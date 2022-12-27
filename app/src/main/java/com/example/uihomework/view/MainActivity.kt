659a543e4dddb1406e021b8086ebcccb9b7f2f71
Gavin Chou <>
2022-12-28 04:15:40 +0800

    add: add resource

--- app/src/main/java/com/example/uihomework/view/MainActivity.kt
+++ app/src/main/java/com/example/uihomework/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.uihomework
+package com.example.uihomework.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.uihomework.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
